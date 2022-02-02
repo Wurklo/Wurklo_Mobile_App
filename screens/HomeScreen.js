1e2e4f68c1d0cd22bccbdc6cd34ac6fafa2a527c
Bobby Keel <bobbykeel76@gmail.com>
2022-02-02 10:01:47 -0600

    stacknavigator setup with homescreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,25 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text style={tw`text-blue-600`}>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+};
+
+export default HomeScreen;
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
