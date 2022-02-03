9686b9a192cd85191876478b115ca1f7e6e51c0e
Bobby Keel <bobbykeel76@gmail.com>
2022-02-03 06:08:51 -0600

    collab+ or solo indicator, clickable icons for favorite, message, share, upvote, downvote

--- components/ProjectPost.js
+++ components/ProjectPost.js
@@ -1,8 +1,40 @@
 import { View, Text, TouchableOpacity, StyleSheet, Image, Button } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+
+const DUMMY_DATA = [
+    {
+        projects: [
+            {
+                id: 0,
+                title: "Building an Electric Car",
+                image: "images/project0.png",
+                description: "I am building a fuel efficient autonomous car. This project will create cars that work with electricity only",
+                upvote: 200,
+                downvote: 50,
+                pay_rate: 500,
+                collab: {
+                    is_collab: true
+                },
+                created: 1620120320
+            }
+        ]
+    }
+]
+
+const project = DUMMY_DATA[0].projects[0]
+
+
 const ProjectPost = () => {
+    const [isUpvote, setIsUpvote] = useState(false);
+    const [isDownvote, setIsDownvote] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [isMessaged, setIsMessaged] = useState(false);
+    const [isShared, setIsShared] = useState(false);
+
+    console.log(project.collab.is_collab)
+    
     return (
         <View style={[
             tw`flex bg-white`,
@@ -14,11 +46,11 @@ const ProjectPost = () => {
                         style={tw`rounded-full h-16 w-16 mx-2`}
                         source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg' }}
                     />
-                    <View style={tw`absolute top-0.5 right-2 bg-blue-500 rounded-full p-1`}>
-                        <Text style={tw`text-white`}>Collab+</Text>
+                    <View style={project.collab.is_collab ? tw`absolute top-0.5 right-2 rounded-full p-1 bg-blue-500` : tw`absolute top-0.5 right-2 rounded-full p-1 bg-yellow-500`}>
+                        <Text style={project.collab.is_collab ? tw`text-white` : ""}>{project.collab.is_collab ? "Collab+" : "Solo"}</Text>
                     </View>
                     <View style={tw`flex w-3/4`}>
-                        <Text style={tw`font-bold`}>Building an electric Car</Text>
+                        <Text style={tw`font-bold`}>{project.title}</Text>
                         <Text style={tw`text-xs`}>7 days ago</Text>
                         <Text style={tw``}>I am building a fuel efficient auto nomous car this is some project ...</Text>
                     </View>
@@ -34,11 +66,11 @@ const ProjectPost = () => {
                 </Text>
             </View>
             <View style={tw`flex-row justify-between mb-1 mx-4`}>
-                <Entypo name="thumbs-down" size={30} color="lightgray" />
-                <Entypo name="thumbs-up" size={30} color="lightgray" />
-                <Entypo name="heart" size={30} color="lightgray" />
-                <Entypo name="message" size={30} color="lightgray" />
-                <MaterialCommunityIcons name="share" size={30} color="lightgray" />
+                <Entypo onPress={() => setIsUpvote(!isUpvote)} name="thumbs-up" size={30} color={isUpvote ? "green" : "lightgray"} />
+                <Entypo onPress={() => setIsDownvote(!isDownvote)} name="thumbs-down" size={30} color={isDownvote ? "red" : "lightgray"} />
+                <Entypo onPress={() => setIsFavorite(!isFavorite)} name="heart" size={30} color={isFavorite ? "red" : "lightgray"} />
+                <Entypo onPress={() => setIsMessaged(!isMessaged)} name="message" size={30} color={isMessaged ? "black" : "lightgray"} />
+                <MaterialCommunityIcons onPress={() => setIsShared(!isShared)} name="share" size={30} color={isShared ? "orange" : "lightgray"} />
             </View>
         </View>
     );
