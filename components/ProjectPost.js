39c11903ddde95f5b7c4f208e55f35abc0fcf603
Bobby Keel <bobbykeel76@gmail.com>
2022-02-03 11:45:15 -0600

    upvote downvoting complete

--- components/ProjectPost.js
+++ components/ProjectPost.js
@@ -2,6 +2,8 @@ import { View, Text, TouchableOpacity, StyleSheet, Image, Button } from 'react-n
 import React, { useState } from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+
+// need to setup moment time display from dummydata
 import moment from 'moment';
 
 const DUMMY_DATA = [
@@ -18,39 +20,47 @@ const DUMMY_DATA = [
                 collab: {
                     is_collab: true
                 },
-                created: 1620120320
+                created: 2643537055
             }
         ]
     }
 ]
 
 const project = DUMMY_DATA[0].projects[0]
-
-const addOne = (num, voteType) => {
-    if (voteType === "upvote") {
-        project.upvote = num += 1;
-    } else {
-        project.downvote = num += 1;
-    }
-}
-
-const subtractOne = (num, voteType) => {
-    if (voteType === "upvote") {
-        project.upvote = num -= 1;
-    } else {
-        project.downvote = num -= 1;
-    }
-}
+const profilePic = 'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg';
 
 const ProjectPost = () => {
     const [isUpvote, setIsUpvote] = useState(false);
     const [isDownvote, setIsDownvote] = useState(false);
     const [isFavorite, setIsFavorite] = useState(false);
-    const [isMessaged, setIsMessaged] = useState(false);
-    const [isShared, setIsShared] = useState(false);
 
-    console.log(project.collab.is_collab)
+    // handle voting
+    const handleVote = (num, voteType) => {
+        if (voteType === "upvote" && isDownvote === false) {
+            project.upvote = num += 1;
+        } else if (voteType === "upvote" && isDownvote === true) {
+            setIsDownvote(false);
+            project.downvote -= 1;
+            project.upvote = num += 1;
+        } else if (voteType === "downvote" && isUpvote === false){
+            project.downvote = num += 1;
+        } else if (voteType === "downvote" && isUpvote === true){
+            setIsUpvote(false);
+            project.upvote -= 1;
+            project.downvote = num += 1;
+        } else {
+            project.downvote = num += 1;
+        }
+    }
 
+    const subtractOne = (num, voteType) => {
+        if (voteType === "upvote") {
+            project.upvote = num -= 1;
+        } else {
+            project.downvote = num -= 1;
+        }
+    }
+    
     return (
         <View style={[
             tw`flex bg-white`,
@@ -60,14 +70,14 @@ const ProjectPost = () => {
                 <View style={tw`flex-row justify-around items-center my-2 relative`}>
                     <Image
                         style={tw`rounded-full h-16 w-16 mx-2`}
-                        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg' }}
+                        source={{ uri: profilePic }}
                     />
                     <View style={tw`absolute top-0.5 right-2 rounded-full p-1${project.collab.is_collab ? ' bg-blue-500' : ' bg-yellow-500'}`}>
                         <Text style={project.collab.is_collab ? tw`text-white` : ""}>{project.collab.is_collab ? "Collab+" : "Solo"}</Text>
                     </View>
                     <View style={tw`flex w-3/4`}>
                         <Text style={tw`font-bold`}>{project.title}</Text>
-                        <Text style={tw`text-xs`}>{moment(project.created).fromNow()}</Text>
+                        <Text style={tw`text-xs`}>{moment().fromNow()}</Text>
                         <Text style={tw``}>{project.description.slice(0, 55)}...</Text>
                     </View>
                 </View>
@@ -83,16 +93,16 @@ const ProjectPost = () => {
             </View>
             <View style={tw`flex-row justify-between mb-1 mx-4`}>
                 <View style={tw`relative`}>
-                    <Entypo onPress={() => isUpvote ? setIsUpvote(false) & subtractOne(project.upvote, "upvote") : setIsUpvote(true) & addOne(project.upvote, "upvote") & setIsDownvote(false)} name="thumbs-up" size={30} color={isUpvote ? "green" : "lightgray"} />
+                    <Entypo onPress={() => isUpvote ? setIsUpvote(false) & subtractOne(project.upvote, "upvote") : setIsUpvote(true) & handleVote(project.upvote, "upvote") & setIsDownvote(false)} name="thumbs-up" size={30} color={isUpvote ? "lightgreen" : "lightgray"} />
                     <Text style={tw`absolute -top-1 -left-2 text-xs text-green-600`}>{project.upvote}</Text>
                 </View>
                 <View style={tw`relative`}>
-                    <Entypo onPress={() => isDownvote ? setIsDownvote(false) & subtractOne(project.downvote, "downvote") : setIsDownvote(true) & addOne(project.downvote, "downvote") & setIsUpvote(false)} name="thumbs-down" size={30} color={isDownvote ? "red" : "lightgray"} />
+                    <Entypo onPress={() => isDownvote ? setIsDownvote(false) & subtractOne(project.downvote, "downvote") : setIsDownvote(true) & handleVote(project.downvote, "downvote") & setIsUpvote(false)} name="thumbs-down" size={30} color={isDownvote ? "pink" : "lightgray"} />
                     <Text style={tw`absolute -bottom-1 -right-2 text-xs text-red-600`}>{project.downvote}</Text>
                 </View>
                 <Entypo onPress={() => setIsFavorite(!isFavorite)} name="heart" size={30} color={isFavorite ? "violet" : "lightgray"} />
-                <Entypo onPress={() => setIsMessaged(!isMessaged)} name="message" size={30} color={isMessaged ? "blue" : "lightgray"} />
-                <MaterialCommunityIcons onPress={() => setIsShared(!isShared)} name="share" size={30} color={isShared ? "orange" : "lightgray"} />
+                <Entypo name="message" size={30} color="skyblue" />
+                <MaterialCommunityIcons name="share" size={30} color="orange" />
             </View>
         </View>
     );
