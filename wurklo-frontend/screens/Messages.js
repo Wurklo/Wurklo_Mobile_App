eecbc17f561adac6c31efd49005fdfd72b5ac53f
Bobby Keel <bobbykeel76@gmail.com>
2022-02-10 22:12:27 -0600

    aded styling to navbar screens

--- wurklo-frontend/screens/Messages.js
+++ wurklo-frontend/screens/Messages.js
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import tw from 'tailwind-react-native-classnames';
 
 const Messages = () => {
   return (
-    <View>
+    <View style={tw`flex-1 justify-center items-center`}>
       <Text>I am messages</Text>
     </View>
   );
--- wurklo-frontend/screens/Post.js
+++ wurklo-frontend/screens/Post.js
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import tw from 'tailwind-react-native-classnames';
 
 const Post = () => {
   return (
-    <View>
+    <View style={tw`flex-1 justify-center items-center`}>
       <Text>I am post creator</Text>
     </View>
   );
