846b723f7ddc3d36b38a13afbba0ab09b391b487
Bobby Keel <bobbykeel76@gmail.com>
2022-03-21 12:13:11 -0500

    profile screen, profile options, header w notifications

---
+++ wurklo-frontend/src/components/ProfileOption.js
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../redux/slices/Login';
+
+const ProfileOption = ({ navigationRoute, option, icon, signout }) => {
+    const navigation = useNavigation();
+    const dispatch = useDispatch();
+    return (
+        <View style={tw`border-b-2 border-gray-200 bg-white`}>
+            <TouchableOpacity
+                style={tw`flex-row justify-between items-center p-2`}
+                onPress={signout ? () => dispatch(setUser(false)) : () => navigation.navigate("Profile")}
+            >
+                <MaterialCommunityIcons name={icon} size={34} color="gray" />
+                <Text style={tw`text-base font-semibold text-gray-500`}>{option}</Text>
+                <MaterialCommunityIcons name="chevron-right" size={34} color="gray" />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default ProfileOption
--- wurklo-frontend/src/screens/Profile.js
+++ wurklo-frontend/src/screens/Profile.js
@@ -1,22 +1,91 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from '../redux/slices/Login';
+import ProfileOption from '../components/ProfileOption';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Entypo } from '@expo/vector-icons';
+
+const username = "Bobby Keel"
+// remove profile pic when user is added
+const profilePic = 'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg';
 
 const Profile = () => {
   const { user } = useSelector((state) => state.user)
   const dispatch = useDispatch();
 
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
-      <TouchableOpacity
-        onPress={() => dispatch(setUser(false))}
-        style={[tw`bg-blue-500 rounded-full mb-2 mx-10`, style.cardShadow]}
-      >
-        <Text style={tw`p-2 px-4 font-semibold text-xl text-white text-center`}>Logout</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={tw`flex-1`} edges={["left", "top", "right"]}>
+      <View style={[tw`flex-row justify-between items-center border-b-2 border-gray-200 bg-white`, style.cardShadow]}>
+        <Image
+          style={tw`rounded-full h-12 w-12 my-2 ml-2`}
+          source={{ uri: profilePic }}
+        />
+        <Text style={[tw`text-xl font-bold`, { color: "#949494" }]}>{username}</Text>
+        <TouchableOpacity style={tw`mr-2`}>
+          <Entypo name="bell" size={30} color="#949494" />
+        </TouchableOpacity>
+        {/* <TouchableOpacity
+          onPress={() => dispatch(setUser(false))}
+          style={[tw`bg-blue-500 rounded-full mr-1`, style.cardShadow]}
+        >
+          <Text style={tw`p-1 px-2 font-semibold text-lg text-white text-center`}>Logout</Text>
+        </TouchableOpacity> */}
+      </View>
+      <ScrollView>
+        <ProfileOption
+          navigationRoute="myAccount"
+          option="My Account"
+          icon="account-cog"
+        />
+        <ProfileOption
+          navigationRoute="wurkerProfile"
+          option="Wurker Profile"
+          icon="briefcase-variant"
+        />
+        <ProfileOption
+          navigationRoute="favorites"
+          option="Favorites Wurkers/Projects"
+          icon="cards-heart"
+        />
+        <ProfileOption
+          navigationRoute="notifications"
+          option="Notifications"
+          icon="bell"
+        />
+        <ProfileOption
+          navigationRoute="billingPayments"
+          option="Billing & Payments"
+          icon="cash-usd"
+        />
+        <ProfileOption
+          navigationRoute="displaySettings"
+          option="Display Settings"
+          icon="monitor-dashboard"
+        />
+        <ProfileOption
+          navigationRoute="languageCountry"
+          option="Language & Country"
+          icon="head-question"
+        />
+        <ProfileOption
+          navigationRoute="helpFeedback"
+          option="Help & Feedback"
+          icon="help"
+        />
+        <ProfileOption
+          navigationRoute="aboutLegal"
+          option="About & Legal"
+          icon="book-open-variant"
+        />
+        <ProfileOption
+          signout={true}
+          option="Sign Out"
+          icon="logout"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -24,14 +93,14 @@ export default Profile;
 
 const style = StyleSheet.create({
   cardShadow: {
-      shadowColor: "#000",
-      shadowOffset: {
-          width: 0,
-          height: 1,
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 1.41,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
 
-      elevation: 10,
+    elevation: 10,
   },
 });
